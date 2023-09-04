115baf0f4c909da1d7d2506e968dc50e5996f89a
Ítalo Soprano <110266373+italosoprano@users.noreply.github.com>
2023-09-03 23:05:51 -0400

    uopdt

--- forca.c
+++ forca.c
@@ -1,59 +1,73 @@
 #include <stdio.h>
 #include <string.h>
 
+    char apelidos[20];
+    char chutes[26];
+    int tentativas = 0;
+
 void abertura() {
     printf("***********************\n");
     printf("*    Jogo de Forca    *\n");
     printf("***********************\n\n");
 }
 
-void chuta(char chutes[26], int tentativas) {
+void chuta(char chutes[26], int* tentativas) {
         char chute;
         printf("Qual a letra ? ");
         scanf(" %c", &chute);
 
-        chutes[tentativas] = chute;
-        tentativas++;
+        chutes[(*tentativas)] = chute;
+        (*tentativas)++;
+}
+
+int jachutou(char letra, char chutes, int tentativas){
+    int achou = 0;
+
+    for(int j = 0 ; j < tentativas ; j++) {
+        if(chutes[j] == letra) {
+            achou = 1;
+            break;
+        }
+    }
+
+    return achou;
+}
+
+void desenhaforca() {
+
+    printf("Você já deu %d chutes\n", tentativas);
+
+    for(int i = 0; i < strlen(apelidos); i++) {
+
+        if(jachutou(apelidos[i])) {
+            printf("%c ", apelidos[i]);
+        } else {
+            printf("_ ");
+        }
+
+    }
+    printf("\n");
+
+}
+
+void escolhepalavra() {
+    sprintf(apelidos, "VAGABUNDO");
 }
 
 int main () {
-    char apelidos[20];
-
-    sprintf(apelidos, "vagabundo");
 
     int acertou = 0;
     int enforcou = 0;
 
-    char chutes[26];
-    int tentativas = 0;
-
     abertura();
+    escolhepalavra();
 
     do {
 
-        for (int i = 0 ; i < strlen(apelidos) ; i++) {
+        desenhaforca();
+        chuta();
 
-            int achou = 0;
-
-            for (int j = 0 ; j < tentativas ; j++) {
-                
-                if(chutes[j] == apelidos[i]){
-                    achou = 1;
-                    break;
-                }
-            }
-            
-            if(achou) {
-                printf("%c ", apelidos[i]);
-            } else {
-                printf("_ ");
-            }
-            
-    }
-
-        printf("\n");
-
-        chuta(chutes, tentativas);
+        tentativas++;
 
     } while (!acertou && !enforcou);
 }
--- rascunho.c
+++ rascunho.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
 
+void somar(int numeros[10]) {
+    int soma = 0;
+
+    for(int i = 0 ; i < 10 ; i++) {
+        soma += numeros[i];
+    }
+
+    printf("%d \n", soma);
+}
+
 int main () {
-    printf("Hello World");
+    int numeros[10] = {44,60,13,9,30,80,51,19,38,8};
+
+    somar(numeros);
 
     return 0;
 }
